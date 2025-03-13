d2d5daa0f5b864dcebf83125cca4977d1c4418f7
Bruno Almeida <brunoalmeida7426@gmail.com>
2025-03-13 16:55:02 +0000

    first commit

---
+++ src/main/java/com/exercise/registo_jogadores/RegistoJogadoresApplication.java
@@ -0,0 +1,13 @@
+package com.exercise.registo_jogadores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistoJogadoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistoJogadoresApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/exercise/registo_jogadores/model/Jogador.java
@@ -0,0 +1,5 @@
+package com.exercise.registo_jogadores.model;
+
+public record Jogador(String nome, String email, String telefone, String codinome, GrupoCodinome grupoCodinome) { 
+    
+}
